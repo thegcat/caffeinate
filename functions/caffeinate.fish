0fd750d2ab16aff34eae5d3a1f11eaa606ffac64
Felix Schäfer <felix@thegcat.net>
2021-12-10 17:09:45 +0100

    omf new plugin caffeinate

---
+++ functions/caffeinate.fish
@@ -0,0 +1,3 @@
+function caffeinate -d "My package"
+  # Package entry-point
+end
---
+++ init.fish
@@ -0,0 +1,6 @@
+# caffeinate initialization hook
+#
+# You can use the following variables in this file:
+# * $package       package name
+# * $path          package path
+# * $dependencies  package dependencies
